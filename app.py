b3242fbbd605551d20e09897a73dc51d27e5545c
trendingorbit <admin@trendingorbit.com>
2023-10-08 12:35:03 +0530

    add job notification

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
+JOBS = [
+    {
+    'id' : 1,
+    'title' : 'Data Analyst',
+    'location' : 'Benganluru, India',
+    'salary' : 'Rs, 1000000',
+    },
+    {
+    'id' : 2,
+    'title' : 'Data Analyst',
+    'location' : 'Delhi, India',
+    'salary' : 'Rs, 1500000',
+    },
+    {
+    'id' : 3,
+    'title' : 'Forntend Engineer ',
+    'location' : 'Remote',
+    'salary' : 'Rs, 1200000',
+    },
+    {
+    'id' : 4,
+    'title' : 'Backend engineer ',
+    'location' : 'Benganluru, India',
+    'salary' : 'Rs, 1000000',
+    },
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html',jobs=JOBS,company_name ='Microsoft')
 
 if __name__ == '__main__':
     app.run(debug=True)
